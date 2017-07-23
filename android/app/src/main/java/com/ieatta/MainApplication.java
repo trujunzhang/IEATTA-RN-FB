a436dd6ee52eadb2b9f081ce5068aec34c9dbd0e
trujunzhang <trujunzhang@gmail.com>
2017-07-23 21:41:27 +0800

    Photo Browser.

--- android/app/src/main/java/com/ieatta/MainApplication.java
+++ android/app/src/main/java/com/ieatta/MainApplication.java
@@ -3,6 +3,7 @@ package com.ieatta;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 
 import io.realm.react.RealmReactPackage;
 
@@ -47,6 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new RCTCameraPackage(),
                     new RealmReactPackage(),
                     new VectorIconsPackage(),
                     new SvgPackage(),
